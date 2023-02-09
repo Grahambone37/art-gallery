fa6b28fec827ec8d97716e3a21fac711aa1e83f0
Grahambone37 <electricgrunt37@gmail.com>
2023-02-08 21:10:14 -0800

    title changes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ import ButtonBar from './button-bar';
 function App() {
   let [artId, setArtId] = useState(12720)
   let [data, setData] = useState({})
+  useEffect(() => {
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+  }, [artId])
   const handleIterate = (e) => {
     setArtId(artId + Number(e.target.value))
   }
-  useEffect(() => {
-    document.title = 'Welcome to ArtWorld'
-    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
-      .then(response => response.json())
-      .then(resData => setData(resData))
-  }, [artId])
   return (
-    <div className="App">
+    <div className="App" title={document.title = data.title || 'Welcome to ArtWorld'}>
       <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
       <ButtonBar handleIterate={handleIterate} />
     </div>
